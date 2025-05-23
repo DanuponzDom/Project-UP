27905a7dd6c7276f32371103ef72577f1d6e6736
DanuponzDom <Volatile_1998@hotmail.com>
2025-05-23 20:45:16 +0700

    Change Port > 5000

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ app.use('/repairlists', repairlistRoute);
 app.use('/repairs', repairRoute);
 
 //Start Server
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
